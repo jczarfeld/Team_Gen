c7068444f8d35eadff1562e77336a09d8a45f67c
John Osterfeld <jczarfeld@gmail.com>
2021-07-21 16:40:09 -0400

    might have to backtrack from this one, tried a async and await

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const Employee = require("./Employee");
-const Egnineer = require("./Engineer");
-const Manager = require("./Manager");
-const Intern = require("./Intern");
+const Employee = require("./lib/Employee");
+const Egnineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
 const fs = require("fs");
 
 const inquirer = require("inquirer");
@@ -45,85 +45,103 @@ const generateHTML = (answers) =>
 </html>`;
 
 // questions in the terminal for user input
-menu = () => {
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?',
-          },
+function menu() {
+        inquirer
+            .prompt([{
+                    type: 'input',
+                    name: 'name',
+                    message: 'What is your name?',
+                },
 
-        {
-            type: "input",
-            name: "id",
-            message: "What is your id?"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is your e-mail address?"
-        },
-        {
-            type: "list",
-            name: "role",
-            message: "What is your role?",
-            choices: ["Engineer", "Intern", "Manager"]
-        },
-        
-       
-
-
-        //takes user input and prints onto the html layout onto a new file
-
-    ])
-    .then(val => {
-        if(val.role="Egnineer") {
-            inquirer
-            .prompt([
                 {
-                    type:"input",
-                    name:"github",
-                    message:"What is your github username?"
-                }
-            ])
-        } else if (val.role="Manager") {
-            inquirer
-            .prompt([
+                    type: "input",
+                    name: "id",
+                    message: "What is your id?"
+                },
                 {
-                    type:"input",
-                    name:"officeNumber",
-                    message:"What is your office number?"
-                }
-            ])
-        } else {
-            inquirer
-            .prompt([
+                    type: "input",
+                    name: "email",
+                    message: "What is your e-mail address?"
+                },
                 {
-                    type:"input",
-                    name:"school",
-                    message:"What is your School's name?"
-                }
+                    type: "list",
+                    name: "role",
+                    message: "What is your role?",
+                    choices: ["Engineer", "Intern", "Manager"]
+                },
+
+
+
+
+                //takes user input and prints onto the html layout onto a new file
+
             ])
-        }
-    })
-    .then((answers) => {
-        const htmlPageContent = generateHTML(answers);
+            .then(answers => {
+                if (answers.role = "Engineer") {
+                    inquirer
+                        .prompt([{
+                            type: "input",
+                            name: "github",
+                            message: "What is your github username?"
+                        }])
+                } else if (answers.role = "Manager") {
+                    inquirer
+                        .prompt([{
+                            type: "input",
+                            name: "officeNumber",
+                            message: "What is your office number?"
+                        }])
+                } else {
+                    inquirer
+                        .prompt([{
+                            type: "input",
+                            name: "school",
+                            message: "What is your School's name?"
+                        }])
+                }
+            })
+            .then((answers) => {
+                const htmlPageContent = generateHTML(answers);
 
-        fs.writeFile('dist/index.html', htmlPageContent, (err) =>
-            err ? console.log(err) : console.log('Successfully created index.html!')
-        );
-    });
-}
-
-//require classes 
-//require packages
-
-//use fs to create html and use bootstrap for the styling
+                fs.writeFile('dist/index.html', htmlPageContent, (err) =>
+                    err ? console.log(err) : console.log('Successfully created index.html!')
+                );
+            });
 
 
+        ////////////////////////////////////////
+        //     const inquirer = require("inquirer");
 
-    //create seperate functions for each class creation--call function that asks questions
+        // (async () => {
+        //   const ans1 = await inquirer.prompt([
+        //     {
+        //       type: "input",
+        //       name: "name",
+        //       message: "What is your name?",
+        //       default: "Jake",
+        //     },
+        //   ]);
+        //   const ans2 = await inquirer.prompt([
+        //     {
+        //       type: "confirm",
+        //       name: "summary",
+        //       message: "Is this information correct? Your name is:" + ans1.name,
+        //     },
+        //   ]);
+        //   return { ...ans1, ...ans2 };
+        // })()
+        //   .then(console.log)
+        //   .catch(console.error);
+        // }
+        ///////////////////////////////////////////////////
+        //require classes 
+        //require packages
+
+        //use fs to create html and use bootstrap for the styling
+
+
+
+        //create seperate functions for each class creation--call function that asks questions
 
 
 
@@ -137,4 +155,4 @@ inquirer
 
 
 
-menu();
+        menu();
